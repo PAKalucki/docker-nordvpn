0ee5c23cebdcf9e1e0a4f3b4a8690fe7918db3f6
prka <pakalucki@gmail.com>
2020-09-16 21:49:41 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo ${user} >> /etc/openvpn/auth
 echo ${password} >> /etc/openvpn/auth
 
-if [[ ${1} =~ "random" ]]
+if [[ ${1} == "random" ]]
 then 
   server = $(ls /etc/openvpn/ovpn_${protocol} | shuf -n 1)
   exec /usr/sbin/openvpn "/etc/openvpn/ovpn_${protocol}/${server}"
